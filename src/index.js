64250645174e642a5dcb7198c48e881374e46ec8
Nicole Woodard <woodard@adobe.com>
2019-08-28 13:51:56 -0600

    Add redux store middleware

--- src/index.js
+++ src/index.js
@@ -3,28 +3,16 @@ import appReducer from './store/reducers'
 import { createStore } from 'redux'
 
 const initialState = (localStorage['redux-store']) ?
-	JSON.parse(localStorage['redux-store']) : {}
-	
+	JSON.parse(localStorage['redux-store']) :
+	{}
+
 const store = createStore(appReducer, initialState)
 
-store.subscribe(() => console.log(JSON.stringify(store.getState())))
+window.store = store
+
 store.subscribe(() => {
+
 	const state = JSON.stringify(store.getState())
-	localStorage['redux-store'] = state
-})
+  localStorage['redux-store'] = state
 
-store.dispatch({
-	type: C.ADD_DAY,
-	payload: {
-		resort: 'Mt Shasta',
-		date: '2019-1-12',
-		powder: false,
-		backcountry: true
-	}
 })
-
-store.dispatch({
-	type: C.SET_GOAL,
-	payload: 12
-})
-
