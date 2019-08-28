acdbba879c2d3264534dd1aca7862a9a7eb4035e
Nicole Woodard <woodard@adobe.com>
2019-08-28 11:03:23 -0600

    Add fetch and suggestions reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,32 +1,75 @@
-import C from '../constants';
+import C from '../constants'
 
-export const goal = (state, action) =>
-  action.type === C.SET_GOAL ? parseInt(action.payload) : state;
+export const goal = (state=10, action) => 
+	(action.type === C.SET_GOAL) ? 
+		 parseInt(action.payload) :
+		 state
 
-export const skiDay = (state = null, action) =>
-  action.type === C.ADD_DAY ? action.payload : state;
+export const skiDay = (state=null, action) => 
+  (action.type === C.ADD_DAY) ?
+  	action.payload :
+  	state
 
-export const errors = (state = [], action) => {
-  switch (action.type) {
-    case C.ADD_ERROR:
-      return [...state, action.payload];
-    case C.CLEAR_ERROR:
-      return state.filter((msg, i) => i !== action.payload);
-    default:
-      return state;
+export const errors = (state=[], action) => {
+  switch(action.type) {
+    case C.ADD_ERROR :
+    	return [
+         ...state,
+         action.payload
+    	]
+    case C.CLEAR_ERROR : 
+      return state.filter((message, i) => i !== action.payload)
+  	default: 
+  		return state
   }
-};
+}
 
-export const allSkiDays = (state = [], action) => {
-  switch (action.type) {
-    case C.ADD_DAY :
-      const hasDay = state.some(
-        hasDay => hasDay.date === payload.date
-      );
-      return hasDay ? state : [...state, skiDay(null, action)]
+export const allSkiDays = (state=[], action) => {
+
+  switch(action.type) {
+
+    case C.ADD_DAY : 
+
+      const hasDay = state.some(skiDay => skiDay.date === action.payload.date)
+
+      return (hasDay) ?
+         state :
+         [
+           ...state,
+           skiDay(null, action)
+         ].sort((a, b) => new Date(b.date) - new Date(a.date))
+
     case C.REMOVE_DAY :
-        return state.filter(skiDay => skiDay.date !== action.payload)
-    default :
+
+      return state.filter(skiDay => skiDay.date !== action.payload)     
+
+    default:
       return state
   }
-};
+
+  
+}
+
+export const fetching = (state=false, action) => {
+  switch(action.type) {
+    case C.FETCH_RESORT_NAMES :
+      return state ? state : !state
+    case C.CANCEL_FETCHING :
+      return state ? !state : state
+    case C.CHANGE_SUGGESTIONS :
+        return state ? !state : state
+    default:
+      return state
+  }
+}
+
+export const suggestions = (state=[], action) => {
+  switch(action.type) {
+    case C.CHANGE_SUGGESTIONS :
+      return state ?  action.payload : state
+    case C.CLEAR_SUGGESTIONS :
+      return []
+    default:
+      return state
+  }
+}
